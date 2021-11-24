dbdfb967bd9127194a38175acdc28e0fa7b49fbc
jhclaura <linkinmonkey@gmail.com>
2021-11-24 14:46:12 -0500

    Add Netlify deploy dashboard

---
+++ cms-sanity/parts/dashboardConfig.js
@@ -0,0 +1,25 @@
+export default {
+  widgets: [
+    {
+      name: 'netlify',
+      options: {
+        title: 'My Netlify deploys',
+        sites: [
+          //   {
+          //     title: 'Sanity Studio',
+          //     apiId: 'xxxxx-yyyy-zzzz-xxxx-yyyyyyyy',
+          //     buildHookId: 'xxxyyyxxxyyyyxxxyyy',
+          //     name: 'sanity-gatsby-blog-20-studio',
+          //   },
+          {
+            title: 'Portfolio site',
+            apiId: '68b024ff-245c-4549-99d7-4e83179f4765',
+            buildHookId: '619e4df02019c75e350b219d',
+            // name: 'jhclaura-portfolio',
+            // url: 'https://my-sanity-deployment.com',
+          },
+        ],
+      },
+    },
+  ],
+}
