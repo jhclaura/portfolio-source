3cd05ad20a2955418767ca0fb4ef2e6c30dc75be
jhclaura <linkinmonkey@gmail.com>
2022-04-06 13:01:54 -0400

    Update video.js

--- cms-sanity/schemas/video.js
+++ cms-sanity/schemas/video.js
@@ -13,5 +13,24 @@ export default {
       type: 'text',
       title: 'Caption',
     },
+    {
+      name: 'widthPercentage',
+      type: 'number',
+      title: 'Width Percentage',
+      description:
+        '100 for full width of the parent container, 50 for half width',
+    },
+    {
+      name: 'backgroundColor',
+      type: 'color',
+      title: 'Background Color',
+      description: 'Color to fill the background of the non 100% width video',
+    },
   ],
+  preview: {
+    select: {
+      title: 'url',
+      description: 'caption',
+    },
+  },
 }
